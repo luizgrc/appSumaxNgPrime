93ca62723b3369d8d72d429985e25d3330f0cf7f
Luis García <lgarcia@sintad.pe>
2019-12-04 08:48:02 -0500

    chart.js npm inicializar

--- src/app/pages/orden/reporte/reporte.component.ts
+++ src/app/pages/orden/reporte/reporte.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'reporte',
@@ -6,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reporte.component.scss']
 })
 export class ReporteComponent implements OnInit {
-
+  chart: any;
   constructor() { }
 
   ngOnInit() {
+    //   this.chart = new Chart('canvas', {
+    //     type: this.tipo,
+    //     data: {
+    //       labels: fechas,
+    //       datasets: [
+    //         {
+    //           label: 'Cantidad',
+    //           data: cantidades,
+    //           borderColor: '#3cba9f',
+    //           fill: false,
+    //           backgroundColor: [
+    //             'rgba(255, 99, 132, 0.2)',
+    //             'rgba(54, 162, 235, 0.2)',
+    //             'rgba(255, 206, 86, 0.2)',
+    //             'rgba(75, 192, 192, 0.2)',
+    //             'rgba(153, 102, 0, 0.2)',
+    //             'rgba(255, 159, 64, 0.2)'
+    //           ]
+    //         }
+    //       ]
+    //     },
+    //     options: {
+    //       legend: {
+    //         display: false
+    //       },
+    //       scales: {
+    //         xAxes: [{
+    //           display: true
+    //         }],
+    //         yAxes: [{
+    //           display: true
+    //         }],
+    //       }
+    //     }
+    //   });
+    // });
   }
+
 
 }
